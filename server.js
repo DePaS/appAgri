afa47f0584dcf1bc6162d0b8dc504ad7f16febb4
depas <depasx@gmail.com>
2020-12-31 18:57:30 +0100

    speriamo funziosi

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const { connect } = require('http2');
 var mysql = require('mysql');
 const passport = require('passport')
 
+app.use(express.static("public"));
+
 var con = mysql.createConnection({
     host: 'depas.c9zdk5ptcfcq.eu-central-1.rds.amazonaws.com',
     user: 'depas',
